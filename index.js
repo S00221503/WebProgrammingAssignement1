e657e5b57fc7b3f35d6ae6e82fb8b59ec88dc34d
Adam Gleeson <S00221503@atu.ie>
2023-11-23 17:23:36 +0000

    UpdatedTestingAndFixes

--- index.js
+++ index.js
@@ -11,19 +11,6 @@ const weather = require('./routes/Weather')
 const app = express()
 const port = process.env.PORT || 3001
 
-/*
-//Calling API
-app.get('/weather', (req, res) => {
-    WeatherData.find({}, (err, data) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json(data);
-    });
-  });
-  //End of calling API
-*/
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
