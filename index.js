bcd6b9cad9fd967d43a69f8d221242c3177addf7
Adam Gleeson <S00221503@atu.ie>
2023-11-24 22:28:34 +0000

    AddedLinkToAngular

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 
 require('dotenv').config();
 require('./database');
@@ -11,9 +12,18 @@ const weather = require('./routes/Weather')
 const app = express()
 const port = process.env.PORT || 3001
 
+//Configure Cors options
+var corsOptions = {
+   origin: 'http://localhost:4200',
+ optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+   }
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.use(express.json());
+
+   //Cors middleware
+   app.use( cors(corsOptions));
 
 app.use('/api/v1/weather', weather)
